78945ee05bf3db11e1b3c6e2f4b9ad4e9e451958
Samuel Oliveira <samueldevbr@gmail.com>
2020-04-09 11:11:57 -0300

    Domain: Validating request for route store new routes on quote domain

--- php/src/App/Domain/Quote/Http/Controllers/RouteController.php
+++ php/src/App/Domain/Quote/Http/Controllers/RouteController.php
@@ -3,13 +3,17 @@ namespace App\Domain\Quote\Http\Controllers;
 
 use App\Infrastructure\Http\Controller;
 use App\Infrastructure\Http\Request;
+use App\Infrastructure\Validation\Rules\IsNumeric;
+use App\Infrastructure\Validation\Rules\Required;
 
 class RouteController extends Controller
 {
 	public function store()
 	{
-		$request = Request::capture();
-
-		var_dump($request->input('data'));
+		$validated = $this->validate([
+			'from' => [new Required],
+			'to' => [new Required],
+			'price' => [new Required, new IsNumeric],
+		]);
 	}
 }
